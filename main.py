c658c0dccd00b59e8ff2fce37456bfd9762544da
Краснокутский Олег Ильич (4037673) <okrasnokutskii@dev.vtb>
2022-06-23 10:08:24 +0300

    [NOTASK] fix leave private chat

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ from utils import MyBot
 mybot = MyBot()
 bot, dp = mybot.get_bot()
 
+COMMANDS_MSG = {
+    'start': 'Хуификатор запущен',
+    'set_message_length': 'Варианты длины хуифицируемых предложений: ',
+    'leave_chat': 'Хуификатор покидает вас',
+    'leave_chat_error': 'Нельзя покинуть личку',
+    'stop': 'Хуификатор остановлен'
+}
+
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
         types.BotCommand("start", "Запустить хуификатор!"),
@@ -20,24 +28,27 @@ async def set_default_commands(dp):
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
     mybot.set_cnt("1")
-    await message.answer("Хуификатор запущен")
+    await message.answer(COMMANDS_MSG['start'])
     await set_default_commands(dp)
 
 @dp.message_handler(commands=['stop'])
 async def cmd_stop(message: types.Message):
     mybot.set_cnt("0")
-    await message.answer("Хуификатор остановлен")
+    await message.answer(COMMANDS_MSG['stop'])
 
 @dp.message_handler(commands=['leave_chat'])
 async def cmd_leave_chat(message: types.Message):
-    await message.answer("Хуификатор покидает вас")
-    await asyncio.sleep(1)
-    await bot.leave_chat(message.chat.id)
+    if message.chat.type == 'private':
+        await message.answer(COMMANDS_MSG['leave_chat_error'])
+    else:
+        await message.answer(COMMANDS_MSG['leave_chat'])
+        await asyncio.sleep(1)
+        await bot.leave_chat(message.chat.id)
 
 @dp.message_handler(commands=['set_message_length'])
 async def cmd_set_message_length(message: types.Message):
     mybot.set_cnt(message.text)
-    await message.answer("Варианты длины хуифицируемых предложений: {0}".format(mybot.get_cnt()))
+    await message.answer(COMMANDS_MSG['set_message_length'] + str(mybot.get_cnt()))
 
 @dp.message_handler(lambda message: len(message.text.split()) in mybot.get_cnt())
 async def answer(message: types.Message):
